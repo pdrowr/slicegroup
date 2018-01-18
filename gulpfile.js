15cfc049949cf01fbcdc798aad1f63d602f0e00a
Enrique <comisionado.falcon@gmail.com>
2018-01-18 17:53:14 -0400

    Cambios inciales para comenzar con la rama enrique

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,7 @@ var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence');
 var merge = require('merge-stream');
-var bourbon = require('bourbon').includePaths;
-var neat = require('bourbon-neat').includePaths;
-
+var rucksack = require('gulp-rucksack');
 //initialize server
 var browserSync = require('browser-sync').create();
 gulp.task('browserSync', function() {
@@ -107,9 +105,8 @@ gulp.task('js', function() {
 //Compile .scss to .css
 gulp.task('compile_sass', function(){
   return gulp.src('app/scss/**/*.scss')
-    .pipe(sass({
-      includePaths: [bourbon, neat]
-    })) // Converts Sass to CSS with gulp-sass
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(rucksack())
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(browserSync.reload({
       stream: true
