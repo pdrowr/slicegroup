4112f9160982b04c6a35140011f4dd7b6548e160
pdrowr <pdrowr@gmail.com>
2018-01-18 10:46:20 -0300

    ADDED font awesome icons and briefing views

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var runSequence = require('run-sequence');
 var merge = require('merge-stream');
 var bourbon = require('bourbon').includePaths;
 var neat = require('bourbon-neat').includePaths;
+
 //initialize server
 var browserSync = require('browser-sync').create();
 gulp.task('browserSync', function() {
