fb1e6b9e710ba25ca2294e44540c2ff3f811691e
Enrique <comisionado.falcon@gmail.com>
2018-01-23 14:38:54 -0400

    Cambios en el footer

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,1 +1,83 @@
+$('#next-1').click(function() {
+  $('#user-dates').css('left', '0')
+})
 
+$('#back-1').click(function() {
+  $('#user-dates').css('left', '-100%')
+})
+
+$('#next-2').click(function() {
+  $('#domain').css('left', '0')
+})
+
+$('#back-2').click(function() {
+  $('#domain').css('left', '-100%')
+})
+
+$('#next-3').click(function() {
+  $('#plan').css('left', '0')
+})
+
+$('#back-3').click(function() {
+  $('#plan').css('left', '-100%')
+})
+
+$('.card').click(function(){
+  $(this).find('i').toggleClass('rotate')
+})
+
+$('#yes').click(function() {
+  $('#no').prop('checked', false);
+  $('#have-domain').css('display', 'block')
+  $('#search-domain').css('display', 'none')
+})
+
+$('#no').click(function() {
+  $('#yes').prop('checked', false);
+  $('#have-domain').css('display', 'none')
+  $('#search-domain').css('display', 'block')
+})
+
+$('#sidebar-trigger').click(function(event) {
+  $('#sidebar').css('right', '0')
+});
+
+$('#close-sidebar').click(function(event) {
+  $('#sidebar').css('right', '-100%')
+});
+
+$('#sidebar').find('a').click(function() {
+  $('#sidebar').css('right', '-100%')
+})
+
+// social
+
+$('#next-social-1').click(function() {
+  $('#social-media').css('left', '0')
+})
+
+$('#back-social-1').click(function() {
+  $('#social-media').css('left', '-100%')
+})
+
+$('.social-card').click(function() {
+  $(this).find('.toggle').toggleClass('item-hidden')
+  var color = $(this).attr('data-color')
+
+  // $(this).css({
+  //   'background': color,
+  //   'color': '#fff'
+  // })
+})
+
+$('#next-social-2').click(function() {
+  $('#plan-social').css('left', '0')
+})
+
+$('#next-social-2').click(function() {
+  $('#plan-social').css('left', '0')
+})
+
+$('#back-social-2').click(function() {
+  $('#plan-social').css('left', '-100%')
+})
